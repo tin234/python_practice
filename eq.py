49e4384cb0db216595199627ee279b8206233095
Alexey Golodnykh <tin234@mail.ru>
2024-11-21 10:44:48 +0300

    Add some

--- eq.py
+++ eq.py
@@ -1,9 +1,16 @@
-person1 = {"name": "Alice", "age": 30}
-person2 = {"name": "Alice", "age": 30}
-person3 = {"name": "Bob", "age": 25}
+class Person:
+    def __eq__(self, value: object) -> bool: 
+        print("in eq class")
+        return True
+
+
+p1 = Person()
+p2 = Person()
+print(p1 == p2)
 
 def compare_people(person1, person2):
     return person1["name"] == person2["name"] and person1["age"] == person2["age"]
 
 print(compare_people(person1, person2))  # True
 print(compare_people(person1, person3))  # False
+
